242dfa5fe90c07f1f2944de9c83d7043a5f46677
M1txY <113921255+M1txY@users.noreply.github.com>
2023-05-03 12:46:22 +0200

    json update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 const paniercount = document.getElementById("paniercount");
 
 
-number = 100; // le nombre de produits dans le panier
+number = 3; // le nombre de produits dans le panier
 
 if (number > 0)  {
     paniercount.textContent = number; 
-}if (number > 9) {
-    paniercount.textContent = "9+";
 }else {
     paniercount.textContent = 0;
     paniercount.style.display = "none";
