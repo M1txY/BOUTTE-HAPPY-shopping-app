91d2ef784914ccdeae2e45da11540a4758d86db2
M1txY <113921255+M1txY@users.noreply.github.com>
2023-05-07 00:34:26 +0200

    panier front + anim et btn select et envoi des produit dans le localstorage

--- js/product.js
+++ js/product.js
@@ -34,7 +34,6 @@ selectbou_casque.addEventListener('click', function () {
         });
 });
 
-// bouton select telephone
 
 selectbou_telephone.addEventListener('click', function () {
     fetch(baseurl + "/smartphones")
@@ -63,7 +62,6 @@ selectbou_telephone.addEventListener('click', function () {
         });
 });
 
-// bouton select ordinateur
 
 selectbou_ordinateur.addEventListener('click', function () {
     fetch(baseurl + "/ordinateurs")
@@ -92,7 +90,6 @@ selectbou_ordinateur.addEventListener('click', function () {
         });
 });
 
-// bouton select accessoire
 
 selectbou_accessoire.addEventListener('click', function () {
     fetch(baseurl + "/accessoires")
@@ -120,4 +117,158 @@ selectbou_accessoire.addEventListener('click', function () {
             console.log(error);
         });
 });
+
+selectbou_all.addEventListener('click', function () {
+    fetch(baseurl + "/smartphones")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            reponstjson.innerHTML = "";
+            for (let i = 0; i < data.length; i++) {
+                reponstjson.innerHTML += `
+                <div class="product">
+                    <div class="product_img">
+                        <img src="${data[i].img_1}" alt="image produit">
+                    </div>
+                    <div class="product_info">
+                        <h3>${data[i].name}</h3>
+                        <p>${data[i].price}€</p>
+                        <button class="addpanier" title="${data[i].id}">Ajouter au panier</button>
+                    </div>
+                </div>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    fetch(baseurl + "/casques")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                reponstjson.innerHTML += `
+                <div class="product">
+                    <div class="product_img">
+                        <img src="${data[i].img_1}" alt="image produit">
+                    </div>
+                    <div class="product_info">
+                        <h3>${data[i].name}</h3>
+                        <p>${data[i].price}€</p>
+                        <button class="addpanier" title="${data[i].id}">Ajouter au panier</button>
+                    </div>
+                </div>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    fetch(baseurl + "/ordinateurs")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                reponstjson.innerHTML += `
+                <div class="product">
+                    <div class="product_img">
+                        <img src="${data[i].img_1}" alt="image produit">
+                    </div>
+                    <div class="product_info">
+                        <h3>${data[i].name}</h3>
+                        <p>${data[i].price}€</p>
+                        <button class="addpanier" title="${data[i].id}">Ajouter au panier</button>
+                    </div>
+                </div>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    fetch(baseurl + "/accessoires")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                reponstjson.innerHTML += `
+                <div class="product">
+                    <div class="product_img">
+                        <img src="${data[i].img_1}" alt="image produit">
+                    </div>
+                    <div class="product_info">
+                        <h3>${data[i].name}</h3>
+                        <p>${data[i].price}€</p>
+                        <button class="addpanier" title="${data[i].id}">Ajouter au panier</button>
+                    </div>
+                </div>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
+
+
+
+
+reponstjson.addEventListener('click', function (e) {
+    if (e.target.classList.contains('addpanier')) {
+        let id = e.target.getAttribute('title');
+        let panier = localStorage.getItem('panier');
+        if (panier == null) {
+            panier = [];
+        } else {
+            panier = JSON.parse(panier);
+        }
+        let productIndex = panier.findIndex(p => p.id === id);
+        if (productIndex === -1) {
+            panier.push({ id: id, quantity: 1 });
+        } else {
+            panier[productIndex].quantity++;
+        }
+        localStorage.setItem('panier', JSON.stringify(panier));
+        let paniercount = document.getElementById("paniercount");
+        paniercount.textContent = panier.reduce((acc, p) => acc + p.quantity, 0);
+    }
+});
+
+
+let panier = localStorage.getItem('panier');
+let paniercount = document.getElementById("paniercount");
+
+if (panier == null || JSON.parse(panier).length == 0) {
+  paniercount.style.display = 'none';
+} else {
+  paniercount.textContent = JSON.parse(panier).reduce((acc, p) => acc + p.quantity, 0);
+  
+  paniercount.style.display = 'flex';
+}
+
+
+
+
+const panierctn = document.querySelector('.panierctn');
+const panierbtn = document.querySelector('.panierbtn');
+const icon = document.querySelector('.changeicon');
+let panierOuvert = false;
+
+panierbtn.addEventListener('click', function () {
+    if (panierOuvert) {
+        panierctn.style.transform = "translateX(100%)";
+        icon.src = "img/sac-de-courses.png";
+        paniercount.style.display = 'flex';
+    } else {
+        panierctn.style.transform = "translateX(0%)";
+        icon.src = "img/cross.png";
+        paniercount.style.display = 'none'
+    }
+    panierOuvert = !panierOuvert;
+}
+);
 
